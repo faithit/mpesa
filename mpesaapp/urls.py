7fddf80c1511f3379a4c134da4a1203faf10163c
faithit <faithittrainer@gmail.com>
2025-07-18 14:32:38 +0300

    pay

--- mpesaapp/urls.py
+++ mpesaapp/urls.py
@@ -5,5 +5,6 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pay/',views.lipa_na_mpesa_online,name='pay'),
-    path('callback/',views.callback,name='callback')
+    path('callback/',views.callback,name='callback'),
+    path('paymentinfo/',views.payment_info,name='paymentinfo')
 ]
--- mpesaapp/views.py
+++ mpesaapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django_daraja.mpesa.core import MpesaClient
 
@@ -7,16 +7,18 @@ from django_daraja.mpesa.core import MpesaClient
 # Create your views here.
 def index(request):
     return render(request, 'pay.html')
+def payment_info(request):
+    return render(request, 'paymentinfo.html')
 def lipa_na_mpesa_online(request):
     if request.method == "POST":
-        phone_number=request.POST[("phone")]
+        phone_number=request.POST[("abcd")]
         amount=int(request.POST[("amount")])
         account_reference="TechWise"
         transaction_desc="school fees payment"
-        callback_url="https://3f88aa9c9a4f.ngrok-free.app/callback"
+        callback_url="https://3f395e3dd5be.ngrok-free.app/callback"
         cl=MpesaClient()
         response =cl.stk_push(phone_number,amount,account_reference,transaction_desc,callback_url)
-        return  HttpResponse(response)
+        return redirect('paymentinfo')
     return HttpResponse("invalid request")
 @csrf_exempt
 def callback(request):
